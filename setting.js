2c29f7522da30258b9dab5da33503416697e0903
silvermd1 <silvermd13@gmail.com>
2025-11-06 22:28:53 +0500

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'SILVER-MD BOT',
+  author: '‎',
+  botName: "SILVER-MD Bot",
+  botOwner: 'TALHA', // Your name
+  ownerNumber: '923106367029', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.0",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
